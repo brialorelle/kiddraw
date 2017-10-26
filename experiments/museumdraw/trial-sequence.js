9dac496200e2f8c5831421ff4794655b5f862787
Bria Long <brialorelle@gmail.com>
2017-10-26 16:59:50 -0700

    basic trial structure intact

--- experiments/museumdraw/trial-sequence.js
+++ experiments/museumdraw/trial-sequence.js
@@ -6,37 +6,41 @@ Oct 26 2017
 */
 
 $(document).ready(function() {	
-
 	console.log('document ready');
-	$('#ready').click(function(){	
+})
+
+$('#ready').click(function(){	
 		console.log('clicked ready button');
-        setTimeout(function() {showCue();},1000); 
-        setTimeout(function() {hideCue();},2000); 
+        setTimeout(function() {showCue();},1000);
+        setTimeout(function() {hideCue();},4000); 
+        setTimeout(function() {showSubmit();},10000);
 		timestamp_cueOnset = new Date().getTime();
-	})
+})
 
-	function showCue() {
-		$('#ready').fadeOut('fast');
-		$('#cue').fadeIn('fast');
-	}
+function showCue() {
+	$('#ready').fadeOut('fast');
+	$('#cue').fadeIn('fast'); //text cue associated with trial
+	$('#cueVideo').show(); //show video html - this can be a variable later?
+	videojs('cueVideo').play(); //get the video element and play it
+}
 
-	function hideCue() {
-		$('#cue').fadeOut('fast');
-		$('#ready').fadeIn('fast');	
-	}
+function hideCue() {
+	$('#cue').fadeOut('fast'); // fade out cue
+}
 
-	function showSubmit() {
-		$('#submit').fadeIn('fast');
-	}
+function showSubmit() {
+	$('#submit').fadeIn('fast');
+}
 
-	function showSlide(id) {
-	  // Hide all slides
-	  $(".slide").hide();
-	  // Show just the slide we want to show
-	  $("#"+id).show();
-	}
+function nextTrial() {
+	console.log('clicked submit');
+	// CLEAR SKETCHPAD AND LOAD DATA //
+	$('#submit').fadeOut('fast'); // fade out submit button
+	$('#ready').fadeIn('fast'); // fade in ready
+	// LOG DATA SOMEHOW //
+	// GET NEXT CUE AND VIDEO //
+}
 
-});
 
 
 
