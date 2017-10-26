ed73bc2d003dd55991b1956944d0d8a70a66d368
Judy Fan <judithfan@gmail.com>
2017-10-26 11:04:55 -0700

    add trial-sequence handlers

---
+++ experiments/museumdraw/trial-sequence.js
@@ -0,0 +1,18 @@
+/* 
+
+Handles dynamic elements of museumdraw task
+Oct 26 2017
+
+*/
+
+function showCue() {
+	$('#cue').fadeIn('fast');
+}
+
+function showSubmit() {
+	$('#submit').fadeIn('fast');
+}
+
+
+
+
