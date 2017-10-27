40df242df01bab65e8023cc8e7721551bdd58b04
Bria Long <brialorelle@gmail.com>
2017-10-26 21:00:58 -0700

    Now dynamically changes cues and videos based on stimListTest

---
+++ experiments/museumdraw/stimListTest.js
@@ -0,0 +1,5 @@
+stimListTest = [{"category": "cat", "video": "videoTest.mp4"},
+			{"category": "apple","video": "videoTest2.mp4"},
+			{"category": "dog","video": "videoTest.mp4"},
+			{"category": "ball","video": "videoTest2.mp4"},
+			]
--- experiments/museumdraw/trial-sequence.js
+++ experiments/museumdraw/trial-sequence.js
@@ -4,37 +4,62 @@ Handles dynamic elements of museumdraw task
 Oct 26 2017
 
 */
+curTrial=0 // global variable
 
 $(document).ready(function() {	
 	console.log('document ready');
 })
 
-$('#ready').click(function(){	
+$('#ready').click(function(){
 		console.log('clicked ready button');
         setTimeout(function() {showCue();},1000);
         setTimeout(function() {hideCue();},4000); 
-        setTimeout(function() {showSubmit();},10000);
+        setTimeout(function() {showSubmit();},8000);
 		timestamp_cueOnset = new Date().getTime();
 })
+ 
 
 function showCue() {
 	$('#ready').fadeOut('fast');
 	$('#cue').fadeIn('fast'); //text cue associated with trial
 	$('#cueVideo').show(); //show video html - this can be a variable later?
-	videojs('cueVideo').play(); //get the video element and play it
+	playVideo();
 }
 
 function hideCue() {
 	$('#cue').fadeOut('fast'); // fade out cue
-	$('#sketchpad').fadeIn('fast'); // fade in sketchpad 
+	$('#cueVideo').hide(); //show video html - this can be a variable later?
+	$('#sketchpad').fadeIn('fast'); // fade in sketchpad  here?
 }
 
 function showSubmit() {
 	$('#submit').fadeIn('fast');
 }
 
+// video player functions
+function playVideo(){
+  videojs("cueVideo").ready(function(){
+  var myPlayer = this;
+  myPlayer.play();
+	});
+}
+
+
+function loadNextVideo(){
+  var player=videojs('cueVideo');
+  player.pause();
+  console.log(stimListTest[curTrial].video)
+  player.src({ type: "video/mp4", src: "videos/" + stimListTest[curTrial].video });
+  player.load();
+}
+
+
 function nextTrial() {
+	curTrial++
 	console.log('clicked submit');
+	loadNextVideo(curTrial)
+	document.getElementById("cue").innerHTML = "Can you draw a "  + stimListTest[curTrial].category;
+
 	// CLEAR SKETCHPAD AND LOAD DATA //
 	$('#submit').fadeOut('fast'); // fade out submit button
 	$('#ready').fadeIn('fast'); // fade in ready
