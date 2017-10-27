4b6dd51c9f14fe09efbe99babf26ca4edacf0298
Bria Long <brialorelle@gmail.com>
2017-10-26 22:25:03 -0700

    Built in placeholder intro/age/thank you slides, etc.

--- experiments/museumdraw/trial-sequence.js
+++ experiments/museumdraw/trial-sequence.js
@@ -7,34 +7,61 @@ Oct 26 2017
 
 // To integrate:
 // HTML divs: Starting welcome page, age page (push age data), thank you page
-// JS: ifthen: Do you want to draw more? If no, thank you page, If yes, check curTrial relative to maxTrials.
 // JS: clear and save sketchpad data (and push to online database after each trial)
+// Convert onclick to also ontouch to work with ipad
+// CSS: Make sure sizing works well on an iPad
 // and much more...
 
 curTrial=0 // global variable, trial counter
+maxTrials=4 // could come from settings file of some kind, hard coded for now.
 
 $(document).ready(function() {	
 	console.log('document ready');
 })
 
+function startExp(){
+	$('#Welcome').fadeOut('fast'); // hide intro screen
+	$('#getAge').fadeIn('fast'); // fade in ready button
+}
+
+function endExp(){
+	$('#mainExp').fadeOut('fast'); 
+	$('#Thanks').fadeIn('fast'); // hide intro screen
+}
+
+function startDrawing(){
+	$('#getAge').fadeOut('fast'); // fade in ready button
+	$('#mainExp').fadeIn('fast'); // fade in ready button
+	$('#ready').fadeIn('fast'); // fade in ready button
+}
+
 $('#ready').click(function(){
 		console.log('clicked ready button');
+		$('#ready').fadeOut('fast');
+		$('#allDone').fadeOut('fast');
         setTimeout(function() {showCue();},1000); 
         setTimeout(function() {hideCue();},4000);  // Take cues away after 4s?
         setTimeout(function() {showSubmit();},8000); // some minimum amount of time before "I'm done button"
 		timestamp_cueOnset = new Date().getTime();
 })
+
+$('#allDone').click(function(){
+		console.log('clicked all done');
+		$('#ready').fadeOut('fast');
+		$('#allDone').fadeOut('fast');
+        endExp();
+})
  
 function showCue() {
-	$('#ready').fadeOut('fast');
+	
 	$('#cue').fadeIn('fast'); //text cue associated with trial
-	$('#cueVideo').show(); //show video div 
+	$('#videoDiv').show(); //show video div 
 	playVideo();
 }
 
 function hideCue() {
 	$('#cue').fadeOut('fast'); // fade out cue
-	$('#cueVideo').hide(); //show video html - this can be a variable later?
+	$('#videoDiv').hide(); //show video html - this can be a variable later?
 	$('#sketchpad').fadeIn('fast'); // fade in sketchpad  here?
 }
 
@@ -61,16 +88,23 @@ function loadNextVideo(){
 function nextTrial() {
 	curTrial++
 	console.log('clicked submit');
-	loadNextVideo(curTrial)
-	document.getElementById("cue").innerHTML = "Can you draw a "  + stimListTest[curTrial].category;
-	// data.sketchData = GET SKETCHPAD DATA
-	// data.category = category
-	// data.video = video
-	// data.submitTime?
-	// CLEAR SKETCHPAD 
-	$('#submit').fadeOut('fast'); // fade out submit button
-	$('#ready').fadeIn('fast'); // fade in ready
-	// GET NEXT CUE AND VIDEO //
+	if (curTrial<maxTrials){
+		loadNextVideo(curTrial)
+		document.getElementById("cue").innerHTML = "Can you draw a "  + stimListTest[curTrial].category;
+		// data.sketchData = GET SKETCHPAD DATA
+		// data.category = category
+		// data.video = video
+		// data.submitTime?
+		// CLEAR SKETCHPAD 
+		$('#submit').fadeOut('fast'); // fade out submit button
+		$('#ready').fadeIn('fast'); // fade in ready
+		$('#allDone').fadeIn('fast'); 
+		// GET NEXT CUE AND VIDEO //
+	}
+	else {
+		endExperiment();
+	}
+
 }
 
 
