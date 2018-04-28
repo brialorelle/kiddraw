b38bb132d56560c3f168f9385ce5f8469ba768f3
Judy Fan <judithfan@gmail.com>
2018-04-28 10:48:39 -0700

    cleanup imagenet scraping script

--- data/python/download_imagenet_datasets.py
+++ data/python/download_imagenet_datasets.py
@@ -1,35 +1,21 @@
 import os
-import tarfile
-import cPickle
 import itertools
 from urllib2 import urlopen
-from collections import defaultdict
-import networkx
 import numpy as np
-import tabular as tb
-import skdata.larray as larray
-from skdata.data_home import get_data_home
-from bs4 import BeautifulSoup
 import random
 from random import sample
-# from dldata.stimulus_sets.dataset_templates import get_id
-# from dldata.stimulus_sets import dataset_templates
-from joblib import Parallel, delayed
 import pymongo as pm
-import gridfs
-import boto
 import requests
-
 import object_correspondences as oc
 
 def build_imagenet_to_labels_dict():
   # define mapping from imagenet synset to common labels
-  d0 = oc.sketch_obj_correspondences
+  d0 = oc.sketch_imagenet_dict
   synset = []
   labels = []
   for key in d0:
     try:
-        synset.append(d0[key]['imagenet'][0])
+        synset.append(d0[key])
         labels.append(key)
     except:
       pass
@@ -37,33 +23,21 @@ def build_imagenet_to_labels_dict():
   imagenet_to_labels = dict(zip(synset,labels))
   return imagenet_to_labels
 
-def get_list_of_eitz_synsets():
-  d0 = oc.sketch_obj_correspondences
+def get_list_of_synsets():
+  d0 = oc.sketch_imagenet_dict
   synset = []
   for key in d0:
     try:
-      synset.append(d0[key]['imagenet'][0])
+      synset.append(d0[key])
     except:
       pass
   return synset
 
-def deborkify_eitz_names():
-  d0 = oc.sketch_obj_correspondences
-  borked_keys = d0.keys()
-  doctify = dict(zip(borked_keys,borked_keys))
-  doctify['car (sedan)'] = 'car'
-  doctify['computer monitor'] = 'monitor'
-  doctify['mouse (animal)'] = 'mouse'
-  doctify['santa claus'] = 'santa'
-  doctify['space shuttle'] = 'space-shuttle'
-  doctify['standing bird'] = 'standing-bird'
-  return doctify
-
 def download_images_by_synset(synsets, num_per_synset=100, path=None,
                               imagenet_username='jefan', accesskey='f5f789c3fb79bfc5e76237ac3feb55b4e959b0ff'):
   """
-  Downloads images by synset, like it says. 
-  Takes in a list of synsets, and optionally number of photos per synset, and saves images in a directory called photos 
+  Downloads images by synset, like it says.
+  Takes in a list of synsets, and optionally number of photos per synset, and saves images in a directory called photos
   """
   path = os.path.join(os.getcwd(),'photos')
   if not os.path.exists(path):
@@ -78,11 +52,10 @@ def download_images_by_synset(synsets, num_per_synset=100, path=None,
             '&username=' + imagenet_username + \
             '&accesskey=' + accesskey + \
             '&release=latest'
-      print i
-      print url
+      print '{} | {}'.format(i, url)
       label = imagenet_to_labels[s]
-      deborked_dict = deborkify_eitz_names() # deborkify eitz names in some cases
-      label = deborked_dict[label]
+      SIDICT = oc.sketch_imagenet_dict
+      label = SIDICT[label]
       url_file = urlopen(url)
       counter = 0
       for f in url_file:
@@ -105,3 +78,12 @@ def download_images_by_synset(synsets, num_per_synset=100, path=None,
             pass
 
 
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_per_synset', type=int, default=10)
+    args = parser.parse_args()
+
+    ## get list of synsets
+    synset_list = get_list_of_synsets()
+    download_images_by_synset(synset_list,num_per_synset=args.num_per_synset)
