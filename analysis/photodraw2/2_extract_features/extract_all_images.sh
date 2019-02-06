fa14af765a7217960b81b7fa9050c1104339e029
Bria Long <brialorelle@gmail.com>
2019-02-05 16:44:41 -0800

    modify feature extraction scripts to handle condition data for photodraw2

---
+++ analysis/photodraw2/2_extract_features/extract_all_images.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python extract_features.py --data='/data5/bria/kiddraw_datasets/photodraw2_020519/' --layer_ind=6 --cohort='kid' --ext="png" --spatial_avg=True --dataset='photodraw2_020519'
