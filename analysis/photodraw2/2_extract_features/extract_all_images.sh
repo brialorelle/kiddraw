a8ed57a48bdaad1d3eeb76cd6df6abff807b6153
Bria Long <brialorelle@gmail.com>
2020-02-03 16:31:48 -0800

    photodraw / photodraw2 experiments/analyses removed and sent to new repository (devphotodraw)

--- analysis/photodraw2/2_extract_features/extract_all_images.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-python extract_features.py --data='/data3/bria/kiddraw_datasets/photodraw2_020519/' --layer_ind=6 --cohort='kid' --ext="png" --spatial_avg=True --dataset='photodraw2_020519'
-
-python extract_features.py --data='/data3/bria/kiddraw_datasets/photodraw2_030719/' --layer_ind=6 --cohort='kid' --ext="png" --spatial_avg=True --dataset='photodraw2_030719'
