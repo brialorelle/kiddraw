7eb3f87069eae086671f7ca569174893d4adedfd
Bria Long <brialorelle@gmail.com>
2020-02-03 16:35:22 -0800

    reorganizing folders for rendering museumstation sketches

---
+++ analysis/museumstation/usage_stats/usage_stats/get_usage_short.py
@@ -0,0 +1,42 @@
+
+## libraries
+import pandas as pd
+import time
+import pymongo as pm
+import os
+
+# set input parameters
+iterationName = 'cdm_run_v7'
+num_hours = 400
+
+# set up  connections
+auth = pd.read_csv('../../auth.txt', header = None) # this auth.txt file contains the password for the sketchloop user
+pswd = auth.values[0][0]
+conn = pm.MongoClient('mongodb://stanford:' + pswd + '@127.0.0.1')
+db = conn['kiddraw']
+coll = db[iterationName]
+
+#
+current_milli_time = lambda: int(round(time.time() * 1000))
+x_hours_ago = current_milli_time() - (num_hours*60*60*1000)
+
+
+# get image recs and sessions since certain date
+image_recs = coll.find({'$and': [{'dataType':'finalImage'}, {'endTrialTime': {"$gt": x_hours_ago}}]})
+valid_sessions = coll.find({'endTrialTime': {"$gt": x_hours_ago}}).distinct('sessionId')
+
+# get count and label fior number of images
+numImages = image_recs.count()
+lastImage = image_recs[numImages - 10]
+
+## get date from most recent image
+lastestDate = lastImage['date']
+
+# fiveImagesAgo = image_recs[numImages - 5]
+# recentDate = fiveImagesAgo['date']
+
+
+print 'In the past {} hours, we have {} valid sessions from {} with {} drawings.'.format(num_hours, len(valid_sessions), iterationName, numImages)
+print 'The last drawing was made at {}.'.format(lastestDate)
+
+
