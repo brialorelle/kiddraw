2b42f245dff096adece637fddb98cd6fa92d0d8b
Bria Long <brialorelle@gmail.com>
2020-08-27 15:53:20 -0700

    move cogsci-2018 analysis folder

---
+++ analysis/cogsci-2018/python/extract_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for i in {0..6}
+    do
+	echo 'extracting kid features from layer ' $i
+	python extract_features.py --layer_ind=$i
+	echo 'extracting adult features from layer ' $i
+	python extract_features.py --data='/data2/jefan/quickDraw/png_micro' --layer_ind=$i --cohort='adult'
+    done
+
+
