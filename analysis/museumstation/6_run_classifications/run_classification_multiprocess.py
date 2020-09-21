0c1e8937763cd487187357dd44e2c2ddadd8d1fc
Bria Long <brialorelle@gmail.com>
2020-09-21 14:50:56 -0700

    fix path issue

--- analysis/museumstation/6_run_classifications/run_classification_multiprocess.py
+++ analysis/museumstation/6_run_classifications/run_classification_multiprocess.py
@@ -37,8 +37,8 @@ It will spawn several threads to get predictions from all splits and models.
 
 def load_features(cohort, layer_num):
     layers = ['P1','P2','P3','P4','P5','FC6','FC7']    
-    F = np.load('/data5/bria/kiddraw_datasets/{}/FEATURES_{}_{}_Spatial_True.npy'.format(DATASET,layers[layer_num],cohort))
-    M = pd.read_csv('/data5/bria/kiddraw_datasets/{}/METADATA_{}.csv'.format(DATASET, cohort)) 
+    F = np.load('/data5/bria/{}/FEATURES_{}_{}_Spatial_True.npy'.format(DATASET,layers[layer_num],cohort))
+    M = pd.read_csv('/data5/bria/{}/METADATA_{}.csv'.format(DATASET, cohort)) 
     #  F = np.load('/Users/brialong/Documents/GitHub/kiddraw/analysis/museumstation/feature_space_analyses/features/{}/FEATURES_{}_{}_Spatial_True.npy'.format(DATASET,layers[layer_num],cohort))
     # M = pd.read_csv('/Users/brialong/Documents/GitHub/kiddraw/analysis/museumstation/feature_space_analyses/features/{}/METADATA_{}.csv'.format(DATASET, cohort)) 
     M = M[['label','age','session']]
@@ -59,37 +59,29 @@ def get_classifications(test_index):
     ## get name of model and split type to get predictions for
     test_index = np.asarray([test_index])
     test_index_numeric = test_index[0]
-
     ## append regularization parameters to saving directory
     out_path_specific = OUT_PATH + "_C_" + str(REGULARIZE_PARAM)
-
     ### Load features, balance dataset
     KF, KM = load_features('kid',LAYER_IND)
     X, y, KM_downsampled = balance_dataset(KF,KM)
-
     ## delete test index from test index array from 
     train_indexes = np.asarray(range(0,np.shape(X)[0]))
     train_indexes = np.delete(train_indexes,test_index)
-
     ## get train/test indexes    
     X_train, X_test = X[train_indexes], X[test_index]
     y_train, y_test = y[train_indexes], y[test_index]
- 
     # run model
-    clf = linear_model.LogisticRegression(penalty='l2',C=REGULARIZE_PARAM,tol=.1,solver='sag').fit(X_train, y_train)
-        
+    clf = linear_model.LogisticRegression(penalty='l2',C=REGULARIZE_PARAM,tol=.1,solver='sag').fit(X_train, y_train)    
     # get outputs and save relevant info
     correct_or_not = clf.score(X_test, y_test) # correct or not
     probs = clf.predict_proba(X_test)   # probabilities
     target_label = KM_downsampled['label'].iloc[test_index_numeric] ## target label
     age = KM_downsampled['age'].iloc[test_index_numeric]
     session_id = KM_downsampled['session'].iloc[test_index_numeric]
-    
     # target probability
     target_label_ind = np.where(clf.classes_==target_label)
     prob_array = probs[0,target_label_ind]
     target_label_prob = (prob_array[0,0])
-
     # print it all out in a dataframe so we group metadata with outputs for easy reading into r 
     _data = pd.DataFrame([test_index_numeric, age, target_label, session_id, correct_or_not,target_label_prob])
     _data = _data.transpose()
@@ -99,7 +91,6 @@ def get_classifications(test_index):
     image_probs_2_df = pd.DataFrame(probs)
     image_probs_2_df.columns = clf.classes_ + "_prob"
     out = pd.concat([_data,image_probs_2_df], axis=1)
-
     ## save it out
     if not os.path.exists(out_path_specific):
         os.makedirs(out_path_specific)
